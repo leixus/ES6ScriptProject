7cc6a7a414b85be64cd6dc593425b6eb3c3b6f58
leixu <leixu@quantongfu.com>
2019-01-03 18:12:28 +0800

    老师讲课的方式感觉不对，强行听完，还要开发实战代码

---
+++ app/js/lottery/timer.js
@@ -0,0 +1,5 @@
+class Timer {
+    countdown (end, update, handle) {
+        const now = new Date().getTime();
+    }
+}
--- server/public/js/classObject.js
+++ server/public/js/classObject.js
@@ -55,93 +55,88 @@
 
 	'use strict';
 
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+	// {
+	//     function Point(x, y) {
+	//         this.x = x;
+	//         this.y = y;
+	//     }
+	//
+	//     Point.prototype.toString = function () {
+	//         return '('+ this.x + ',' + this.y + ')';
+	//     };
+	//
+	//     var p = new Point(1, 2);
+	//
+	//     console.log(p.toString());
+	// }
+
 	{
-	    var Point = function Point(x, y) {
-	        this.x = x;
-	        this.y = y;
-	    };
+	    var Person = function () {
+	        function Person() {
+	            var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'leixu';
 
-	    Point.prototype.toString = function () {
-	        return '(' + this.x + ',' + this.y + ')';
-	    };
+	            _classCallCheck(this, Person);
 
-	    var p = new Point(1, 2);
+	            this.name = name;
+	        }
 
-	    console.log(p.toString());
-	}
+	        _createClass(Person, [{
+	            key: 'getPersion',
+	            value: function getPersion() {
+	                this.setPersion(this.name);
+	                return this.name;
+	            }
+	        }, {
+	            key: 'setPersion',
+	            value: function setPersion(name) {
+	                console.log(name);
+	            }
+	        }]);
 
-	{
-	    var Person = function Person() {
-	        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'leixu';
-
-	        _classCallCheck(this, Person);
-
-	        this.name = name;
-	    };
+	        return Person;
+	    }();
 
 	    var v_person = new Person('lei');
 
 	    console.log(v_person.name);
+
+	    v_person.getPersion();
 	}
 
-	{
-	    var _Person = function _Person() {
-	        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'tiandi';
-
-	        _classCallCheck(this, _Person);
-
-	        this.name = name;
-	    };
-
-	    var OnePerson = function (_Person2) {
-	        _inherits(OnePerson, _Person2);
-
-	        function OnePerson() {
-	            _classCallCheck(this, OnePerson);
-
-	            return _possibleConstructorReturn(this, (OnePerson.__proto__ || Object.getPrototypeOf(OnePerson)).apply(this, arguments));
-	        }
-
-	        return OnePerson;
-	    }(_Person);
-
-	    console.log('继承', new OnePerson('镰刀'));
-	}
-
-	{
-	    var _Person3 = function _Person3() {
-	        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'tiandi';
-
-	        _classCallCheck(this, _Person3);
-
-	        this.name = name;
-	    };
-
-	    var _OnePerson = function (_Person4) {
-	        _inherits(_OnePerson, _Person4);
-
-	        function _OnePerson() {
-	            var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'child';
-
-	            _classCallCheck(this, _OnePerson);
-
-	            var _this2 = _possibleConstructorReturn(this, (_OnePerson.__proto__ || Object.getPrototypeOf(_OnePerson)).call(this, name));
-
-	            _this2.type = 'person';
-	            return _this2;
-	        }
-
-	        return _OnePerson;
-	    }(_Person3);
-
-	    console.log('继承', new _OnePerson('锤子'));
-	}
+	// {
+	//     class Person {
+	//         constructor(name = 'tiandi') {
+	//             this.name = name;
+	//         }
+	//     }
+	//
+	//     class OnePerson extends Person {
+	//
+	//     }
+	//
+	//     console.log('继承', new OnePerson('镰刀'));
+	// }
+	//
+	// {
+	//     class Person {
+	//         constructor(name = 'tiandi') {
+	//             this.name = name;
+	//         }
+	//     }
+	//
+	//     class OnePerson extends Person {
+	//         constructor(name = 'child') {
+	//             super(name);
+	//             this.type = 'person'
+	//         }
+	//     }
+	//
+	//     console.log('继承', new OnePerson('锤子'));
+	// }
 
 /***/ })
 
